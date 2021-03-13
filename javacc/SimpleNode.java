b0f4544da906f022d979218ec54ec8dd041ab4b1
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2021-03-13 10:14:55 +0000

    Add Node class

--- javacc/SimpleNode.java
+++ javacc/SimpleNode.java
@@ -4,103 +4,109 @@ import java.lang.RuntimeException;
 import java.util.*;
 
 public class SimpleNode implements Node {
-  protected Node parent;
-  protected Node[] children;
-  protected int id;
-  private final Map<String, String> attributes;
+    protected Node parent;
+    protected Node[] children;
+    protected int id;
+    protected Jmm parser;
+    private final Map<String, String> attributes;
 
-  public SimpleNode(int i) {
-      id = i;
-      this.attributes = new HashMap<String, String>();
-  }
+    public SimpleNode(int i) {
+        id = i;
+        this.attributes = new HashMap<>();
+    }
 
-  public String getKind() {
-      return toString();
-  }
+    public SimpleNode(Jmm p, int i) {
+        this(i);
+        parser = p;
+    }
 
-  public List<String> getAttributes() {
-      return new ArrayList<>(this.attributes.keySet());
-  }
+    public String getKind() {
+        return toString();
+    }
 
-  public void put(String attribute, String value) {
-      this.attributes.put(attribute, value);
-  }
+    public List<String> getAttributes() {
+        return new ArrayList<>(this.attributes.keySet());
+    }
 
-  public String get(String attribute) {
-      return this.attributes.get(attribute);
-  }
+    public void put(String attribute, String value) {
+        this.attributes.put(attribute, value);
+    }
 
-  public List<JmmNode> getChildren() {
-      List<JmmNode> childNodes = new ArrayList<>();
+    public String get(String attribute) {
+        return this.attributes.get(attribute);
+    }
 
-      if(this.children == null) {
+    public List<JmmNode> getChildren() {
+        List<JmmNode> childNodes = new ArrayList<>();
+
+        if(this.children == null) {
           return childNodes;
-      }
+        }
 
-      Collections.addAll(childNodes, this.children);
-      return childNodes;
-  }
+        Collections.addAll(childNodes, this.children);
+        return childNodes;
+    }
 
-  public int getNumChildren() {
-      return jjtGetNumChildren();
-  }
+    public int getNumChildren() {
+        return jjtGetNumChildren();
+    }
 
-  public void add(JmmNode child, int index) {
-      jjtAddChild((Node) child, index);
-  }
+    public void add(JmmNode child, int index) {
+        jjtAddChild((Node) child, index);
+    }
 
-  public void jjtOpen() { }
+    public void jjtOpen() { }
 
-  public void jjtClose() { }
+    public void jjtClose() { }
 
-  public void jjtSetParent(Node n) {
-      parent = n;
-  }
+    public void jjtSetParent(Node n) {
+        this.parent = n;
+    }
 
-  public Node jjtGetParent() {
-      return parent;
-  }
+    public Node jjtGetParent() {
+        return this.parent;
+    }
 
-  public void jjtAddChild(Node n, int i) {
-      if (children == null) {
-          children = new Node[i + 1];
-      } else if (i >= children.length) {
-          Node c[] = new Node[i + 1];
-          System.arraycopy(children, 0, c, 0, children.length);
-          children = c;
-      }
-      children[i] = n;
-  }
+    public void jjtAddChild(Node n, int i) {
+        if (this.children == null) {
+            this.children = new Node[i + 1];
+        } else if (i >= this.children.length) {
+          Node[] c = new Node[i + 1];
+          System.arraycopy(this.children, 0, c, 0, this.children.length);
+            this.children = c;
+        }
+        children[i] = n;
+    }
 
-  public Node jjtGetChild(int i) {
-      return children[i];
-  }
+    public Node jjtGetChild(int i) {
+        return this.children[i];
+    }
 
-  public int jjtGetNumChildren() {
-      return (children == null) ? 0 : children.length;
-  }
+    public int jjtGetNumChildren() {
+        return (this.children == null) ? 0 : this.children.length;
+    }
 
-  public String toString() {
-      return JmmTreeConstants.jjtNodeName[id];
-  }
+    public String toString() {
+        return JmmTreeConstants.jjtNodeName[this.id];
+    }
 
-  public String toString(String prefix) {
-      return prefix + toString();
-  }
+    public String toString(String prefix) {
+        return prefix + toString();
+    }
 
-  public void dump(String prefix) {
-      System.out.println(toString(prefix));
-      if (children != null) {
-          for (int i = 0; i < children.length; ++i) {
-              SimpleNode n = (SimpleNode)children[i];
+    public void dump(String prefix) {
+        System.out.println(toString(prefix));
+        if (this.children != null) {
+          for (int i = 0; i < this.children.length; ++i) {
+              SimpleNode n = (SimpleNode)this.children[i];
               if (n != null) {
                   n.dump(prefix + " ");
               }
           }
-      }
-  }
+        }
+    }
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return this.id;
+    }
 }
