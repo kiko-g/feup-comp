8982b2302c20a9fa61357ec44147dced3921dd57
João Bispo <joaobispo@gmail.com>
2021-04-04 23:03:17 +0100

    Adds .toString() to Symbol and Table

---
+++ src-lib/pt/up/fe/comp/jmm/analysis/table/Symbol.java
@@ -0,0 +1,25 @@
+package pt.up.fe.comp.jmm.analysis.table;
+
+public class Symbol {
+    private final Type type;
+    private final String name;
+
+    public Symbol(Type type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Symbol [type=" + type + ", name=" + name + "]";
+    }
+
+}
