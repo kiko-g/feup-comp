129fbd1a99661a8522216502fed691acfe024424
João Bispo <joaobispo@gmail.com>
2021-04-08 09:30:54 +0100

    Implements .equals() and .hashCode() for Type and Symbol

--- src-lib/pt/up/fe/comp/jmm/analysis/table/Symbol.java
+++ src-lib/pt/up/fe/comp/jmm/analysis/table/Symbol.java
@@ -22,4 +22,41 @@ public class Symbol {
         return "Symbol [type=" + type + ", name=" + name + "]";
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Symbol other = (Symbol) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        return true;
+    }
+
 }
--- src-lib/pt/up/fe/comp/jmm/analysis/table/Type.java
+++ src-lib/pt/up/fe/comp/jmm/analysis/table/Type.java
@@ -22,4 +22,38 @@ public class Type {
         return "Type [name=" + name + ", isArray=" + isArray + "]";
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (isArray ? 1231 : 1237);
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Type other = (Type) obj;
+        if (isArray != other.isArray)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
 }
